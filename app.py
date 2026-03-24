5466046ba5907f4d4de50a26bd8edde1453cb570
Nick Menzhuber <nickmenzhuber@gmail.com>
2026-03-23 22:44:37 -0500

    prep for Railway deploy

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import json
 import unicodedata
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/basement_stacks'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://localhost/basement_stacks')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
